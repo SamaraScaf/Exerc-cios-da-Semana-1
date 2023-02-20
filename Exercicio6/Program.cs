b7535d13c5b4c371829d0c929d6caca452d36402
SamaraScaf <122405994+SamaraScaf@users.noreply.github.com>
2023-02-20 17:04:13 -0300

    Exercício da semana 1 - Tabuada

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,12 @@
+﻿//[M1S01] Ex 6 -  Tabuada
+int numero = 0;
+
+Console.Write("Informe a tabuada que você quer treinar: ");
+numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Segue abaixo a tabuada desejada:");
+
+for (int i =0; i<=10; i=i+1){
+int resultado =  numero*i;
+Console.WriteLine($"{numero} x {i} = {resultado}");
+}
