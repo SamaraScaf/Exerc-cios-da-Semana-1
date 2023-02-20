8a2626750eb2ce94c14976dd4da1bce213d58936
SamaraScaf <122405994+SamaraScaf@users.noreply.github.com>
2023-02-20 13:26:01 -0300

    Exercício da semana 1 - Entrevista

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿//[M1S01] Ex 1 - Entrevista
+Console.WriteLine("Olá, tudo bem? Qual é o seu nome?");
+string nome=Console.ReadLine();
+
+Console.WriteLine("Bem vindo, " + nome + ". Qual é o seu sobrenome?");
+string sobrenome=Console.ReadLine();
+
+Console.WriteLine("Qual é a sua idade?");
+string idade=Console.ReadLine();
+
+Console.WriteLine("Qual é a sua cor favorita?");
+string corfavorita=Console.ReadLine();
+
+Console.WriteLine("Respostas do Usuário");
+Console.WriteLine("Seu nome é " + nome + " " + sobrenome);
+Console.WriteLine("Sua idade é " + idade + " " + "anos");
+Console.WriteLine("Sua cor favorita é " + corfavorita);
