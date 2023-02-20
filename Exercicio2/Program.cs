7ae35548bc8b4f357bf2fb229a7414534b20dc2e
SamaraScaf <122405994+SamaraScaf@users.noreply.github.com>
2023-02-20 13:48:30 -0300

    Exercício da semana 1 - Área do triângulo

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿//[M1S01] Ex 2 - Área do Triângulo 
+int baseTriangulo=0, alturaTriangulo=0;
+
+Console.WriteLine("Calcular a área do triângulo ");
+
+Console.Write("Informe a base do triângulo: ");
+baseTriangulo=int.Parse(Console.ReadLine());
+
+Console.Write("Informe a altura do triângulo: ");
+alturaTriangulo=int.Parse(Console.ReadLine());
+
+int resultado=baseTriangulo*alturaTriangulo/2;
+Console.WriteLine("A área do triângulo é: " + resultado);
+
