0215f90d1138d81f13544606547842d8d9bceb25
SamaraScaf <122405994+SamaraScaf@users.noreply.github.com>
2023-02-20 17:08:52 -0300

    Exercício da semana 1 - Fibonacci

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,14 @@
+﻿// [M1S01] Ex 8 - Fibonacci
+Console.WriteLine("Sequência Fibonacci");
+
+int f1 = 0;
+int f2 = 1;
+int fibo;
+
+Console.Write(1 + ",");
+for (int i = 0; i < 20; i=i+1){
+fibo = f1 + f2;
+Console.Write(fibo + ",");
+f1 = f2;
+f2 = fibo;
+}
